546201db281d295614bca73b569983b19fbd60bd
Steven <willsawesometoolkit@gmail.com>
2019-10-30 15:28:17 +0000

    Add files via upload

--- interfaces/ishelllogger.go
+++ interfaces/ishelllogger.go
@@ -18,11 +18,16 @@ type IShellLogger interface {
 	SetLogLevel(kitlevel.Option)
 	//SetLogPrefix(string)
 
+	LogErrorf(cmd string, message string, data ...interface{})
+	LogWarnf(cmd string, message string, data ...interface{})
+	LogInfof(cmd string, message string, data ...interface{})
+	LogDebugf(cmd string, message string, data ...interface{})
+
 	LogError(cmd string, data ...interface{})
 	LogWarn(cmd string, data ...interface{})
 	LogInfo(cmd string, data ...interface{})
 	LogDebug(cmd string, data ...interface{})
 
-	// This opens a session 
+	// This opens a session
 	OpenSessionFileLog(session ISession) *os.File
 }
